21a74c8891fc3029006954a6b5f5366ea1391113
John W <telestrial@gmail.com>
2021-05-14 00:25:29 -0400

    Nested Conditionals and Truthiness (167-169)
    Nice

---
+++ 05_JavaScript/04_Truthy_Falsy/app.js
@@ -0,0 +1,37 @@
+const userInput = prompt('Enter something');
+
+if (userInput) {
+    console.log('Truthy!')
+} else {
+    console.log('Falsy!')
+}
+
+// if (1) {
+//     console.log('Truthy!')
+// } else {
+//     console.log('Falsy!')
+// }
+
+// if (0) {
+//     console.log('Truthy!')
+// } else {
+//     console.log('Falsy!')
+// }
+
+// if (null) {
+//     console.log('Truthy!')
+// } else {
+//     console.log('Falsy!')
+// }
+
+// if (undefined) {
+//     console.log('Truthy!')
+// } else {
+//     console.log('Falsy!')
+// }
+
+// if (null) {
+//     console.log('Truthy!')
+// } else {
+//     console.log('Falsy!')
+// }
