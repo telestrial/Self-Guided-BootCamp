ec6a32cf82d5138eb3005bd9ac1261a15c126910
John W <telestrial@gmail.com>
2021-06-29 20:52:48 -0400

    More React Basics (39-44)

--- React-The-Complete-Guide/complete-react-guide/src/components/Expenses/ExpenseDate.js
+++ React-The-Complete-Guide/complete-react-guide/src/components/Expenses/ExpenseDate.js
@@ -1,6 +1,7 @@
+import React from "react";
 import "./ExpenseDate.css";
 
-function ExpenseDate(props) {
+const ExpenseDate = (props) => {
   const month = props.date.toLocaleString("en-US", { month: "long" });
   const year = props.date.getFullYear();
   const day = props.date.toLocaleString("en-US", { day: "2-digit" });
@@ -12,6 +13,6 @@ function ExpenseDate(props) {
       <div className="expense-date__day">{day}</div>
     </div>
   );
-}
+};
 
 export default ExpenseDate;
--- React-The-Complete-Guide/complete-react-guide/src/components/Expenses/ExpenseItem.js
+++ React-The-Complete-Guide/complete-react-guide/src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,18 @@
+import React from "react";
 import "./ExpenseItem.css";
+import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
-}
+};
 
 export default ExpenseItem;
