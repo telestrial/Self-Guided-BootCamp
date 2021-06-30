51cd0e7f874eee22923e748eebda589f3e8a7e5c
John W <telestrial@gmail.com>
2021-06-29 23:28:57 -0400

    State And Events Intro (45-52)

--- React-The-Complete-Guide/complete-react-guide/src/App.js
+++ React-The-Complete-Guide/complete-react-guide/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/newExpense/NewExpense";
 
 const App = () => {
   const expenses = [
@@ -33,7 +34,7 @@ const App = () => {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <Expenses expenses={expenses} />
     </div>
   );
