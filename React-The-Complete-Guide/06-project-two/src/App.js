3dcb5f373b9208ece4cdc85ffd0d501e3ad8d49d
John W <telestrial@gmail.com>
2021-07-05 18:15:32 -0400

    project two 3/4 (138-144)

--- React-The-Complete-Guide/06-project-two/src/App.js
+++ React-The-Complete-Guide/06-project-two/src/App.js
@@ -1,16 +1,29 @@
-import { Fragment } from "react";
+import { useState } from "react";
 
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
-    <Fragment>
-      <Header />
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} onHideCart={hideCartHandler} />
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
--- React-The-Complete-Guide/06-project-two/src/components/Layout/HeaderCartButton.js
+++ React-The-Complete-Guide/06-project-two/src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,23 @@
-import CartIcon from "../Cart/CartIcon";
+import { useContext } from "react";
 
+import CartIcon from "../Cart/CartIcon";
+import CartContext from "../../store/cart-context";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const numberOfCartItems = cartCtx.items.reduce((currentNumber, item) => {
+    return currentNumber + item.amount;
+  }, 0);
+
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
