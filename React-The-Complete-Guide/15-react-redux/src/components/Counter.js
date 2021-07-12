dee901680128084e9e15d31281fbb4cccfb4c2c0
John W <telestrial@gmail.com>
2021-07-12 18:13:10 -0400

    Redux Intro End (238-247)

--- React-The-Complete-Guide/15-react-redux/src/components/Counter.js
+++ React-The-Complete-Guide/15-react-redux/src/components/Counter.js
@@ -1,26 +1,27 @@
 import { useSelector, useDispatch } from "react-redux";
 
+import { counterActions } from "../store/counter";
 import classes from "./Counter.module.css";
 
 const Counter = () => {
   const dispatch = useDispatch();
-  const counter = useSelector((state) => state.counter);
-  const show = useSelector((state) => state.showCounter);
+  const counter = useSelector((state) => state.counter.counter);
+  const show = useSelector((state) => state.counter.showCounter);
 
   const incrementHandler = () => {
-    dispatch({ type: "increment" });
+    dispatch(counterActions.increment());
   };
 
   const increaseHandler = () => {
-    dispatch({ type: "increase", amount: 10 });
+    dispatch(counterActions.increase(10));
   };
 
   const decrementHandler = () => {
-    dispatch({ type: "decrement" });
+    dispatch(counterActions.decrement());
   };
 
   const toggleCounterHandler = () => {
-    dispatch({ type: "toggle" });
+    dispatch(counterActions.toggleCounter());
   };
 
   return (
