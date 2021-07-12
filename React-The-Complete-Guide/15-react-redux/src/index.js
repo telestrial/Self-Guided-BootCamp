5106cc91ed0f3c9c8ebeb4896bd22df87b2d1c29
John W <telestrial@gmail.com>
2021-07-11 22:04:10 -0400

    More Redux Intro! (230-237)

--- React-The-Complete-Guide/15-react-redux/src/index.js
+++ React-The-Complete-Guide/15-react-redux/src/index.js
@@ -1,7 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
+import store from "./store/index";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
