bd455638d39e1d1436d3efafeeeaccdaafc0029c
John W <telestrial@gmail.com>
2021-07-14 19:09:08 -0400

    Advanced Redux END (252-263)

--- React-The-Complete-Guide/16-redux-advanced/src/store/cart-slice.js
+++ React-The-Complete-Guide/16-redux-advanced/src/store/cart-slice.js
@@ -5,13 +5,19 @@ const cartSlice = createSlice({
   initialState: {
     items: [],
     totalQuantity: 0,
+    changed: false,
   },
   reducers: {
+    replaceCart(state, action) {
+      state.totalQuantity = action.payload.totalQuantity;
+      state.items = action.payload.items;
+    },
+
     addItemToCart(state, action) {
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
       state.totalQuantity++;
-
+      state.changed = true;
       if (!existingItem) {
         state.items.push({
           id: newItem.id,
@@ -29,7 +35,7 @@ const cartSlice = createSlice({
       const id = action.payload;
       const existingItem = state.items.find((item) => item.id === id);
       state.totalQuantity--;
-
+      state.changed = false;
       if (existingItem.quantity === 1) {
         state.items = state.items.filter((item) => item.id !== id);
       } else {
