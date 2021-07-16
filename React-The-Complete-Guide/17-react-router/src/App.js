8be98034ac58b2cea9eb7693186c40aac4add007
John W <telestrial@gmail.com>
2021-07-16 17:52:45 -0400

    React Router Intro (264-274)

---
+++ React-The-Complete-Guide/17-react-router/src/App.js
@@ -0,0 +1,38 @@
+import { Route, Switch, Redirect } from "react-router-dom";
+
+import Welcome from "./pages/Welcome";
+import Products from "./pages/Products";
+import ProductDetail from "./pages/ProductDetail";
+import MainHeader from "./components/MainHeader";
+
+function App() {
+  return (
+    <div>
+      <MainHeader />
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/welcome" />
+          </Route>
+          <Route path="/welcome">
+            <Welcome />
+          </Route>
+          <Route path="/products" exact>
+            <Products />
+          </Route>
+          <Route path="/products/:productId">
+            <ProductDetail />
+          </Route>
+        </Switch>
+      </main>
+    </div>
+  );
+}
+
+export default App;
+
+// React Routing
+// We conditionally render components based on URL path:
+// our-domain.com/ => Welcome Component
+// our-domain.com/products => Products Component
+// our-domain.com/product-detail/anything => Product Detail Component for any identifier
