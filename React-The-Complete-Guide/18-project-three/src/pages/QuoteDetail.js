69a5ba862dd3265867df22b83328c0e3b8d208e6
John W <telestrial@gmail.com>
2021-07-17 21:17:46 -0400

    Project Three PART I (275-280)

---
+++ React-The-Complete-Guide/18-project-three/src/pages/QuoteDetail.js
@@ -0,0 +1,32 @@
+import { Fragment } from "react";
+import { useParams, Route } from "react-router";
+
+import Comments from "../components/comments/Comments";
+import HighlightedQuotes from "../components/quotes/HighlightedQuote";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Max", text: "Learning React is fun!" },
+  { id: "q2", author: "John", text: "Learn to Curriculum, bro!" },
+  { id: "q3", author: "Internet", text: "Top course ever!" },
+];
+
+const QuoteDetail = () => {
+  const params = useParams();
+
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>No quote found!</p>;
+  }
+
+  return (
+    <Fragment>
+      <HighlightedQuotes text={quote.text} author={quote.author} />
+      <Route path={`/quotes/${params.quoteId}/comments`}>
+        <Comments />
+      </Route>
+    </Fragment>
+  );
+};
+
+export default QuoteDetail;
