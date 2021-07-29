9e9dbd407470bb3b954347d04eee2a4b02a90c34
John W <telestrial@gmail.com>
2021-07-29 10:56:24 -0400

    CSS Animations FINISH (353-363)

--- React-The-Complete-Guide/22-css-animations/src/components/Modal/Modal.js
+++ React-The-Complete-Guide/22-css-animations/src/components/Modal/Modal.js
@@ -1,17 +1,34 @@
 import React from "react";
+import CSSTransition from "react-transition-group/CSSTransition";
 
 import "./Modal.css";
 
-const modal = (props) => {
-  const cssClasses = ["Modal", props.show ? "ModalOpen" : "ModalClosed"];
+const animationTiming = {
+  enter: 400,
+  exit: 1000,
+};
 
+const modal = (props) => {
   return (
-    <div className={cssClasses.join(" ")}>
-      <h1>A Modal</h1>
-      <button className="Button" onClick={props.closed}>
-        Dismiss
-      </button>
-    </div>
+    <CSSTransition
+      mountOnEnter
+      unmountOnExit
+      in={props.show}
+      timeout={animationTiming}
+      classNames={{
+        enter: "",
+        enterActive: "ModalOpen",
+        exit: "",
+        exitActive: "ModalClosed",
+      }}
+    >
+      <div className="Modal">
+        <h1>A Modal</h1>
+        <button className="Button" onClick={props.closed}>
+          Dismiss
+        </button>
+      </div>
+    </CSSTransition>
   );
 };
 
