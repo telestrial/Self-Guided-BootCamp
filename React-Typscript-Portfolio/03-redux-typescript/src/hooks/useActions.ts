b03067d9c767d62532af72ded5a483f63f6d8ab0
John W <telestrial@gmail.com>
2021-08-04 16:29:49 -0400

    Typescript and Redux (21-44)

---
+++ React-Typscript-Portfolio/03-redux-typescript/src/hooks/useActions.ts
@@ -0,0 +1,10 @@
+import { useDispatch } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { actionCreators } from '../state';
+
+export const useActions = () => {
+  const dispatch = useDispatch();
+
+  return bindActionCreators(actionCreators, dispatch);
+  // Ouputs (kind of): {searchRepositories: dispatch(searchRepositories())}
+};
