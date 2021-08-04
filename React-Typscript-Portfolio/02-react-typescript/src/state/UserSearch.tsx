83e46dd5ba844b18d3bd6795d7c1877f3a87e95c
John W <telestrial@gmail.com>
2021-08-04 11:54:04 -0400

    State (3-14)

---
+++ React-Typscript-Portfolio/02-react-typescript/src/state/UserSearch.tsx
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+
+const users = [
+  { name: 'John', age: 31 },
+  { name: 'Alex', age: 20 },
+  { name: 'Sarah', age: 29 },
+];
+
+const UserSearch: React.FC = () => {
+  const [name, setName] = useState('');
+  const [user, setUser] = useState<{ name: string; age: number } | undefined>();
+
+  const onClick = () => {
+    const foundUser = users.find((user) => {
+      return user.name === name;
+    });
+
+    setUser(foundUser);
+  };
+
+  return (
+    <div>
+      User Search
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <button onClick={onClick}>Find User</button>
+      <div>
+        {user && user.name}
+        {user && user.age}
+      </div>
+    </div>
+  );
+};
+
+export default UserSearch;
