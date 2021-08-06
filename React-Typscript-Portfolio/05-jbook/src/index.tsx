2827a7ff22d25e250277cf767f00a2e2ae734578
John W <telestrial@gmail.com>
2021-08-06 16:13:10 -0400

    Bundling and Dynamic Fetching (61-80)

--- React-Typscript-Portfolio/05-jbook/src/index.tsx
+++ React-Typscript-Portfolio/05-jbook/src/index.tsx
@@ -1,26 +1,44 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
 
 import * as esbuild from 'esbuild-wasm';
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 
 const App = () => {
   const [input, setInput] = useState('');
   const [code, setCode] = useState('');
+  const ref = useRef<any>();
 
   const startService = async () => {
-    const service = await esbuild.startService({
+    ref.current = await esbuild.startService({
       worker: true,
       wasmURL: '/esbuild.wasm',
     });
-    console.log(service);
   };
 
   useEffect(() => {
     startService();
   }, []);
 
-  const onClickHandler = () => {
-    console.log(input);
+  const onClickHandler = async () => {
+    if (!ref.current) {
+      return;
+    }
+
+    const result = await ref.current.build({
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()],
+      define: {
+        'process.env.NODE_ENV': '"production"',
+        global: 'window',
+      },
+    });
+
+    // console.log(result);
+
+    setCode(result.outputFiles[0].text);
   };
 
   return (
