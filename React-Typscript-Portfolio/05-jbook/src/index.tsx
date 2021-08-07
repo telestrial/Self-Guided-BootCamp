6502a8a1c06dee642745ab3318b75ad6f6d96c22
John W <telestrial@gmail.com>
2021-08-07 13:57:44 -0400

    Caching Packages (81-95)

--- React-Typscript-Portfolio/05-jbook/src/index.tsx
+++ React-Typscript-Portfolio/05-jbook/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 
 import * as esbuild from 'esbuild-wasm';
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
+import { fetchPlugin } from './plugins/fetch-plugin';
 
 const App = () => {
   const [input, setInput] = useState('');
@@ -12,7 +13,7 @@ const App = () => {
   const startService = async () => {
     ref.current = await esbuild.startService({
       worker: true,
-      wasmURL: '/esbuild.wasm',
+      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
     });
   };
 
@@ -29,7 +30,7 @@ const App = () => {
       entryPoints: ['index.js'],
       bundle: true,
       write: false,
-      plugins: [unpkgPathPlugin()],
+      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
       define: {
         'process.env.NODE_ENV': '"production"',
         global: 'window',
