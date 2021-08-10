94c5e68dd856bfa03fdc3c319062d2d765a281f3
John W <telestrial@gmail.com>
2021-08-10 13:41:29 -0400

    Redux Pattern and Immer (180-198)

---
+++ React-Typscript-Portfolio/05-jbook/src/state/store.ts
@@ -0,0 +1,24 @@
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import reducers from './reducers';
+import { ActionType } from './action-types';
+
+export const store = createStore(reducers, {}, applyMiddleware(thunk));
+
+store.dispatch({
+  type: ActionType.INSERT_CELL_BEFORE,
+  payload: {
+    id: null,
+    type: 'code',
+  },
+});
+
+store.dispatch({
+  type: ActionType.INSERT_CELL_BEFORE,
+  payload: {
+    id: null,
+    type: 'text',
+  },
+});
+
+console.log(store.getState());
