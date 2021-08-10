a0edb9fac6bfd340a1e81a746eb57a1042cba930
John W <telestrial@gmail.com>
2021-08-09 20:32:31 -0400

    Resizable Components and Error Handling (138-167)

--- React-Typscript-Portfolio/05-jbook/src/components/code-editor.tsx
+++ React-Typscript-Portfolio/05-jbook/src/components/code-editor.tsx
@@ -70,7 +70,7 @@ const CodeEditor: React.FC<CodeEditorProps> = ({ onChange, initialValue }) => {
         value={initialValue}
         theme="dark"
         language="javascript"
-        height="500px"
+        height="100%"
         options={{
           wordWrap: 'on',
           minimap: { enabled: false },
--- React-Typscript-Portfolio/05-jbook/src/components/preview.tsx
+++ React-Typscript-Portfolio/05-jbook/src/components/preview.tsx
@@ -1,44 +1,66 @@
 import React, { useRef, useEffect } from 'react';
 
+import './preview.css';
+
 interface PreviewProps {
   code: string;
+  err: string;
 }
 
+// It seems like a more clever way to do this would be to import this from an
+// html file and then somehow stringify it. It's a mess to debug like so.
 const html = `
 <html>
-    <head></head>
-    <body>
-        <div id="root"></div>
-        <script>
-            window.addEventListener('message', (event) => {
-                try {
-                    eval(event.data);
-                } catch (err) {
-                    const root = document.querySelector('#root')
-                    root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>'
-                    throw err;
-                }
-            }, false);
-        </script>
-    </body>
+  <head>
+    <style> html {background-color: white;}</style>
+  </head>
+  <body>
+    <div id="root"></div>
+    <script>
+      const handleError = (err) => {
+        const root = document.querySelector('#root')
+        root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>'
+        console.log(err);
+      };
+
+      window.addEventListener('error', (event) => {
+        event.preventDefault();
+        handleError(event.error);
+      });
+
+      window.addEventListener('message', (event) => {
+        try {
+          eval(event.data);
+        } catch (err) {
+          handleError(err);
+        }
+      }, false);
+    </script>
+  </body>
 </html>
 `;
 
-const Preview: React.FC<PreviewProps> = ({ code }) => {
+const Preview: React.FC<PreviewProps> = ({ code, err }) => {
   const iframe = useRef<any>();
 
   useEffect(() => {
     iframe.current.srcDoc = html;
-    iframe.current.contentWindow.postMessage(code, '*');
+
+    setTimeout(() => {
+      iframe.current.contentWindow.postMessage(code, '*');
+    }, 50);
   }, [code]);
 
   return (
-    <iframe
-      title="preview"
-      ref={iframe}
-      sandbox="allow-scripts"
-      srcDoc={html}
-    />
+    <div className="preview-wrapper">
+      <iframe
+        title="preview"
+        ref={iframe}
+        sandbox="allow-scripts"
+        srcDoc={html}
+      />
+      {err && <div className="preview-error">{err}</div>}
+    </div>
   );
 };
 
