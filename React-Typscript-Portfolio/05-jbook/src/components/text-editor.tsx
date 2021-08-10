358790ea5692e0ebda91a7bdd4ce02008fe7e3f0
John W <telestrial@gmail.com>
2021-08-10 10:39:14 -0400

    Markdown Editor (168-179)

---
+++ React-Typscript-Portfolio/05-jbook/src/components/text-editor.tsx
@@ -0,0 +1,48 @@
+import { useState, useEffect, useRef } from 'react';
+
+import MDEditor from '@uiw/react-md-editor';
+
+import './text-editor.css';
+
+const TextEditor: React.FC = () => {
+  const [editing, setEditing] = useState(false);
+  const [value, setValue] = useState('# Header');
+  const ref = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const listener = (event: MouseEvent) => {
+      if (
+        ref.current &&
+        event.target &&
+        ref.current.contains(event.target as Node)
+      ) {
+        return;
+      }
+
+      setEditing(false);
+    };
+    document.addEventListener('click', listener, { capture: true });
+
+    return () => {
+      document.removeEventListener('click', listener, { capture: true });
+    };
+  }, []);
+
+  if (editing) {
+    return (
+      <div className="text-editor" ref={ref}>
+        <MDEditor value={value} onChange={(v) => setValue(v || '')} />
+      </div>
+    );
+  }
+
+  return (
+    <div className="text-editor card" onClick={() => setEditing(true)}>
+      <div className="card-content">
+        <MDEditor.Markdown source={value} />
+      </div>
+    </div>
+  );
+};
+
+export default TextEditor;
--- React-Typscript-Portfolio/05-jbook/src/index.tsx
+++ React-Typscript-Portfolio/05-jbook/src/index.tsx
@@ -1,13 +1,14 @@
 import ReactDOM from 'react-dom';
 
-import CodeCell from './components/code-cell';
+// import CodeCell from './components/code-cell';
+import TextEditor from './components/text-editor';
 
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 
 const App = () => {
   return (
     <div>
-      <CodeCell />
+      <TextEditor />
     </div>
   );
 };
