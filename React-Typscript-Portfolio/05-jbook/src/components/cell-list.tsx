f73e07ea34f762b78513b62ef46cdf1ce249187f
John W <telestrial@gmail.com>
2021-08-11 13:45:48 -0400

    Cumulative Code Execution (239-253)

--- React-Typscript-Portfolio/05-jbook/src/components/cell-list.tsx
+++ React-Typscript-Portfolio/05-jbook/src/components/cell-list.tsx
@@ -4,6 +4,8 @@ import { useTypedSelector } from '../hooks/use-typed-selector';
 import CellListItem from './cell-list-item';
 import AddCell from './add-cell';
 
+import './cell-list.css';
+
 const CellList: React.FC = () => {
   const cells = useTypedSelector(({ cells: { order, data } }) =>
     order.map((id) => data[id])
@@ -17,7 +19,7 @@ const CellList: React.FC = () => {
   ));
 
   return (
-    <div>
+    <div className="cell-list">
       <AddCell forceVisible={cells.length === 0} previousCellId={null} />
       {renderedCells}
     </div>
