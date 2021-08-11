9c6d62f0bfec7d00681dd64037545d0ecd24f028
John W <telestrial@gmail.com>
2021-08-11 17:56:01 -0400

    Creating a CLI (268-273)

---
+++ React-Typscript-Portfolio/05-jbook/jbook/packages/cli/src/commands/serve.ts
@@ -0,0 +1,13 @@
+import path from 'path';
+import { Command } from 'commander';
+
+import { serve } from 'local-api';
+
+export const serveCommand = new Command()
+  .command('serve [filename]')
+  .description('Open a file for editing')
+  .option('-p, --port <number>', 'port to run server on', '4005')
+  .action((filename = 'notebook.js', options: { port: string }) => {
+    const dir = path.join(process.cwd(), path.dirname(filename));
+    serve(parseInt(options.port), path.basename(filename), dir);
+  });
--- React-Typscript-Portfolio/05-jbook/jbook/packages/local-api/src/index.ts
+++ React-Typscript-Portfolio/05-jbook/jbook/packages/local-api/src/index.ts
@@ -1,3 +1,5 @@
-export default () => {
-  console.log('Server is listening');
+export const serve = (port: number, filename: string, dir: string) => {
+  console.log('serving traffic on port', port);
+  console.log('saving/fetching cells from', filename);
+  console.log('that file is in dir:', dir);
 };
