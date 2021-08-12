b7a138023eeb3b4a2ab207167296127ddeb4c1e5
John W <telestrial@gmail.com>
2021-08-11 20:12:48 -0400

    Local-Only API (274-291)

--- React-Typscript-Portfolio/05-jbook/jbook/packages/cli/src/commands/serve.ts
+++ React-Typscript-Portfolio/05-jbook/jbook/packages/cli/src/commands/serve.ts
@@ -3,11 +3,32 @@ import { Command } from 'commander';
 
 import { serve } from 'local-api';
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 export const serveCommand = new Command()
   .command('serve [filename]')
   .description('Open a file for editing')
   .option('-p, --port <number>', 'port to run server on', '4005')
-  .action((filename = 'notebook.js', options: { port: string }) => {
-    const dir = path.join(process.cwd(), path.dirname(filename));
-    serve(parseInt(options.port), path.basename(filename), dir);
+  .action(async (filename = 'notebook.js', options: { port: string }) => {
+    try {
+      const dir = path.join(process.cwd(), path.dirname(filename));
+      await serve(
+        parseInt(options.port),
+        path.basename(filename),
+        dir,
+        !isProduction
+      );
+      console.log(
+        `Opened ${filename}. Navigate to http://localhost:${options.port} to edit the file.`
+      );
+    } catch (err) {
+      if (err.code === 'EADDRINUSE') {
+        console.log(
+          'Port is in use. Try running on a different port with "-p <port number>".'
+        );
+      } else {
+        console.log("Here's the problem", err);
+      }
+      process.exit(1);
+    }
   });
---
+++ React-Typscript-Portfolio/05-jbook/jbook/packages/local-api/src/routes/cells.ts
@@ -0,0 +1,42 @@
+import express from 'express';
+import fs from 'fs/promises';
+import path from 'path';
+
+interface Cell {
+  id: string;
+  content: string;
+  type: 'text' | 'code';
+}
+
+export const createCellsRouter = (filename: string, dir: string) => {
+  const router = express.Router();
+  router.use(express.json());
+
+  const fullPath = path.join(dir, filename);
+
+  router.get('/cells', async (req, res) => {
+    try {
+      const result = await fs.readFile(fullPath, { encoding: 'utf-8' });
+      res.send(JSON.parse(result));
+    } catch (err) {
+      if (err.code === 'ENOENT') {
+        await fs.writeFile(fullPath, '[]', 'utf-8');
+        res.send([]);
+      } else {
+        throw err;
+      }
+    }
+  });
+
+  router.post('/cells', async (req, res) => {
+    // Take the list of cells from the request object.
+    // serialize them
+    const { cells }: { cells: Cell[] } = req.body;
+    // Write the cells into the file
+    await fs.writeFile(fullPath, JSON.stringify(cells), 'utf-8');
+
+    res.send({ status: 'ok' });
+  });
+
+  return router;
+};
