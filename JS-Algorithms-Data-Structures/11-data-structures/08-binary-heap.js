f02a8228608bd8fbb224fbfa58a86a9922e07531
John W <telestrial@gmail.com>
2021-08-21 12:17:09 -0400

    Binary Heaps Part I (182-187)

---
+++ JS-Algorithms-Data-Structures/11-data-structures/08-binary-heap.js
@@ -0,0 +1,72 @@
+// Similar to a binary search tree, but it has different rules.
+// In a MaxBinaryHeap, parent nodes are always larger than child nodes. In a
+// MinBinaryHeap, parent nodes are always smaller than child nodes. There is no order to
+// the left or the right.
+
+// You can use built-in structures to represent binary heaps.
+
+// Representing a heap in an array is based on a parent->child index relationship of
+// 2n + 1, 2n + 2. And child->parent of ((n-1) / 2).floor()
+
+class MaxBinaryHeap {
+  constructor() {
+    this.values = [];
+  }
+  insert(value) {
+    this.values.push(value);
+    return this.bubbleUp();
+  }
+  bubbleUp() {
+    let idx = this.values.length - 1;
+    const element = this.values[idx];
+    while (idx > 0) {
+      let parentIdx = Math.floor((idx - 1) / 2);
+      let parent = this.values[parentIdx];
+      if (element <= parent) break;
+      this.values[parentIdx] = element;
+      this.values[idx] = parent;
+      idx = parentIdx;
+    }
+    return this.values;
+  }
+  extractMax() {
+    const max = this.values[0];
+    const end = this.values.pop();
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      this.sinkDown();
+    }
+    return max;
+  }
+  sinkDown() {
+    let idx = 0;
+    const length = this.values.length;
+    const element = this.values[0];
+    while (true) {
+      let leftChildIdx = 2 * idx + 1;
+      let rightChildIdx = 2 * idx + 2;
+      let leftChild, rightChild;
+      let swap = null;
+
+      if (leftChildIdx < length) {
+        leftChild = this.values[leftChildIdx];
+        if (leftChild > element) {
+          swap = leftChildIdx;
+        }
+      }
+      if (rightChildIdx < length) {
+        rightChildIdx = this.values[rightChildIdx];
+        if (
+          (swap === null && rightChild > element) ||
+          (swap !== null && rightChild > leftChild)
+        ) {
+          swap = rightChildIdx;
+        }
+      }
+      if (swap === null) break;
+      this.values[idx] = this.values[swap];
+      this.values[swap] = element;
+      idx = swap;
+    }
+  }
+}
