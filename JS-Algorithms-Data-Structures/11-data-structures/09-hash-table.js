79a8dd022a904967a9619ed5a38e8b4235f978fd
John W <telestrial@gmail.com>
2021-08-23 11:15:19 -0400

    Hash Tables (193-205)

---
+++ JS-Algorithms-Data-Structures/11-data-structures/09-hash-table.js
@@ -0,0 +1,93 @@
+// Hash Table
+// Used to store key-value pairs. Unordered. They excel at finding values, adding new
+// values, and removing values!
+
+// Python has dictionaries. JS has objects (with some restrictions) and maps. Java, Go,
+// and Scala have maps. Ruby has hashes.
+
+// Hash tables rely on....hash functions. Hash functions take data of an arbitrary size
+// and spits out data of a fixed size.
+
+// Not a great hash function, but okay for the strings we'll hash:
+const hash = (key, arrayLen) => {
+  let total = 0;
+  let WEIRD_PRIME = 31;
+  for (let i = 0; i < Math.min(key.length, 100); i++) {
+    let char = key[i];
+    let value = char.charCodeAt(0) - 96;
+    total = (total * WEIRD_PRIME + value) % arrayLen;
+  }
+  return total;
+};
+
+// But what happens during a collision?
+
+// If using an array, we could just store multiple values at an index. Then, when
+// referenced, we could simply loop over the items in that array.
+
+// We could also simply move up or down in index to find an empty index if the one we want
+// to store at has something in it. There are complications here around how you'd go
+// about retrieving the item in this case.
+
+class HashTable {
+  constructor(size = 53) {
+    this.keyMap = new Array(size);
+  }
+  _hash(key) {
+    let total = 0;
+    let WEIRD_PRIME = 31;
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+      let char = key[i];
+      let value = char.charCodeAt(0) - 96;
+      total = (total * WEIRD_PRIME + value) % this.keyMap.length;
+    }
+    return total;
+  }
+  set(key, value) {
+    let index = this._hash(key);
+    if (!this.keyMap[index]) {
+      this.keyMap[index] = [];
+    }
+    this.keyMap[index].push([key, value]);
+  }
+  get(key) {
+    let index = this._hash(key);
+    if (this.keyMap[index]) {
+      for (let i = 0; i < this.keyMap[index].length; i++) {
+        if (this.keyMap[index][i][0] === key) {
+          return this.keyMap[index][i][1];
+        }
+      }
+    }
+    return undefined;
+  }
+  values() {
+    let valuesArr = [];
+    for (let i = 0; i < this.keyMap.length; i++) {
+      if (this.keyMap[i]) {
+        for (let j = 0; j < this.keyMap[i].length; j++) {
+          if (!valuesArr.includes(this.keyMap[i][j][1]))
+            valuesArr.push(this.keyMap[i][j][1]);
+        }
+      }
+    }
+    return valuesArr;
+  }
+  keys() {
+    let keysArr = [];
+    for (let i = 0; i < this.keyMap.length; i++) {
+      if (this.keyMap[i]) {
+        for (let j = 0; j < this.keyMap[i].length; j++) {
+          if (!keysArr.includes(this.keyMap[i][j][0]))
+            keysArr.push(this.keyMap[i][j][0]);
+        }
+      }
+    }
+    return keysArr;
+  }
+}
+
+// Big O Complexity
+// Insert: O(1)
+// Deletion: O(1)
+// Access: O(1)
