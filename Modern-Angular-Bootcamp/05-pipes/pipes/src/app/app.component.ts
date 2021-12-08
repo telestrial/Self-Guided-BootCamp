d448ebbaf01e0c63e1436276b241bb424778653a
John Williams <telestrial@gmail.com>
2021-12-08 14:03:40 -0500

    Pipes (53-65)

---
+++ Modern-Angular-Bootcamp/05-pipes/pipes/src/app/app.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  name: string = '';
+  date: string = '';
+  amount: number | undefined = undefined;
+  height: number | undefined = undefined;
+  miles: number | undefined = undefined;
+
+  car = {
+    make: 'Toyota',
+    model: 'Camry',
+    year: 200,
+  };
+
+  onNameChange(value: string) {
+    this.name = value;
+  }
+
+  onDateChange(value: string) {
+    this.date = value;
+  }
+
+  onAmountChange(value: string) {
+    this.amount = parseFloat(value);
+  }
+
+  onHeightChange(value: string) {
+    this.height = parseFloat(value);
+  }
+
+  onMilesChange(value: string) {
+    this.miles = parseFloat(value);
+  }
+}
