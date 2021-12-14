1fc1e5e1a91f97e9fd672bdbad35cd3cc902f592
John Williams <telestrial@gmail.com>
2021-12-14 14:27:37 -0500

    Advanced Component Reusability (135-148)

---
+++ Modern-Angular-Bootcamp/08-modules-routing/comps/src/app/mods/mods-home/mods-home.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mods-home',
+  templateUrl: './mods-home.component.html',
+  styleUrls: ['./mods-home.component.css'],
+})
+export class ModsHomeComponent implements OnInit {
+  modalOpen = false;
+  items = [
+    { title: 'Why is the sky blue?', content: 'The sky is blue because it is' },
+    {
+      title: 'What does an orange taste like?',
+      content: 'An orange tastes like an orange',
+    },
+    {
+      title: 'What color is that cat?',
+      content: 'Leave my cat alone, bro-he-mus',
+    },
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onClick() {
+    this.modalOpen = !this.modalOpen;
+  }
+}
