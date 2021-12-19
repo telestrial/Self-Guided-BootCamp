81b5f1547920a1ddc7623707858c0225b9f78d58
John Williams <telestrial@gmail.com>
2021-12-19 14:28:00 -0500

    Typescript and Rxjs (205-210)

--- Modern-Angular-Bootcamp/10-requests-and-security/wsearch/src/app/app.component.ts
+++ Modern-Angular-Bootcamp/10-requests-and-security/wsearch/src/app/app.component.ts
@@ -7,13 +7,13 @@ import { WikipediaService } from './wikipedia.service';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  pages = [];
+  pages: any = [];
 
   constructor(private wikipedia: WikipediaService) {}
 
   onTerm(term: string) {
-    this.wikipedia.search(term).subscribe((response: any) => {
-      this.pages = response.query.search;
+    this.wikipedia.search(term).subscribe((pages) => {
+      this.pages = pages;
     });
   }
 }
