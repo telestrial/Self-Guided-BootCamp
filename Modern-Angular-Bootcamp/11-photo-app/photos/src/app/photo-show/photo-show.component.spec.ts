cb2415eec76246b501a67c2a5c6b08968e2e0f33
John Williams <telestrial@gmail.com>
2021-12-20 10:36:33 -0500

    Photo Fetching App (211-221)

---
+++ Modern-Angular-Bootcamp/11-photo-app/photos/src/app/photo-show/photo-show.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PhotoShowComponent } from './photo-show.component';
+
+describe('PhotoShowComponent', () => {
+  let component: PhotoShowComponent;
+  let fixture: ComponentFixture<PhotoShowComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PhotoShowComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PhotoShowComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ Modern-Angular-Bootcamp/11-photo-app/photos/src/app/photos.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface UnsplashResponse {
+  urls: {
+    regular: string;
+  };
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PhotosService {
+  constructor(private http: HttpClient) {}
+
+  getPhoto() {
+    return this.http.get<UnsplashResponse>(
+      'https://api.unsplash.com/photos/random',
+      {
+        headers: {
+          Authorization:
+            // API Key has been removed here.
+            'Client-ID',
+        },
+      }
+    );
+  }
+}
