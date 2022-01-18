f047b75cb0c7606a114ee79d99431a12f33a3ee4
John Williams <telestrial@gmail.com>
2022-01-18 14:05:30 -0500

    Custom Validators Part I (270-280)

---
+++ Modern-Angular-Bootcamp/14-emailclient/emailclient/src/app/auth/auth.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AuthRoutingModule } from './auth-routing.module';
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
+
+@NgModule({
+  declarations: [SigninComponent, SignupComponent],
+  imports: [CommonModule, AuthRoutingModule, ReactiveFormsModule],
+})
+export class AuthModule {}
