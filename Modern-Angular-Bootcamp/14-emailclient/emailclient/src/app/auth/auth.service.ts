cf9c227ee1be59553d48421f2176e810612324f3
John Williams <telestrial@gmail.com>
2022-01-25 12:38:39 -0500

    Handling Auth Part I (290-299)

--- Modern-Angular-Bootcamp/14-emailclient/emailclient/src/app/auth/auth.service.ts
+++ Modern-Angular-Bootcamp/14-emailclient/emailclient/src/app/auth/auth.service.ts
@@ -1,22 +1,46 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, tap } from 'rxjs';
 
 interface UsernameAvailableResponse {
   available: true;
+}
+
+interface SignupCredentials {
+  username: string;
+  password: string;
+  passwordConfirmation: string;
+}
+
+interface SignupResponse {
+  username: string;
 }
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
+  rootUrl = 'https://api.angular-email.com';
+  signedIn$ = new BehaviorSubject(false);
+
   constructor(private http: HttpClient) {}
 
   usernameAvailable(username: string) {
     return this.http.post<UsernameAvailableResponse>(
-      'https://api.angular-email.com/auth/username',
+      this.rootUrl + '/auth/username',
       {
         username,
       }
     );
+  }
+
+  signUp(credentials: SignupCredentials) {
+    return this.http
+      .post<SignupResponse>(this.rootUrl + '/auth/signup', credentials)
+      .pipe(
+        tap(() => {
+          this.signedIn$.next(true);
+        })
+      );
   }
 }
