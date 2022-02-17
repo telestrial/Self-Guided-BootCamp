677037aaba2e3a94aa6fcbeec8d3b0e38cbef8fd
John Williams <telestrial@gmail.com>
2022-02-17 13:33:26 -0500

    Typescript I (357-369)

---
+++ Modern-Angular-Bootcamp/15-typescript/01-annotations/01-variables.ts
@@ -0,0 +1,56 @@
+let apples: number = 5;
+let speed: string = 'fast';
+let hasName: boolean = true;
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
+
+// Built in objects
+let now: Date = new Date();
+
+// Array
+let colors: string[] = ['red', 'green', 'blue'];
+let myNumbers: number[] = [1, 2, 3, 4, 5];
+let truths: boolean[] = [true, true, false];
+
+// Classes
+class Car {}
+
+let car: Car = new Car();
+
+// Object literal
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20,
+};
+
+// Function
+const logNumber: (i: number) => void = (i: number) => {
+  console.log(i);
+};
+
+// Type inference - typescript figures it out on its own. Must be same line.
+let myName = 'John';
+
+// So when do you use annotations?
+// 1) Function that returns the 'any' type
+const json = '{"x":10, "y": 20}';
+// JSON.parse returns the <any> type unless we specify.
+const coordinates: { x: number; y: number } = JSON.parse(json);
+console.log(coordinates); // {x: 10, y: 20}
+
+// 2) When we declare a variable on one line and initialize it later.
+let words = ['red', 'green', 'blue'];
+let foundWord: boolean;
+
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === 'green') foundWord = true;
+}
+
+// 3) Variable whose type can't be inferred.
+// This isn't necessarily a good approach code-wise.
+let numbers = [-10, -1, 12];
+let numberAboveZero: boolean | number = false;
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > 0) numberAboveZero = numbers[i];
+}
