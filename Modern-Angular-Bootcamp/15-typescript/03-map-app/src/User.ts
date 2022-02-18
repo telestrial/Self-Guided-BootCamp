5520bfa669577c547bee522722ce49a830c5f574
John Williams <telestrial@gmail.com>
2022-02-18 13:27:42 -0500

    Typescript IV (401-416)

--- Modern-Angular-Bootcamp/15-typescript/03-map-app/src/User.ts
+++ Modern-Angular-Bootcamp/15-typescript/03-map-app/src/User.ts
@@ -1,6 +1,8 @@
 import faker from '@faker-js/faker';
 
-export class User {
+import { Mappable } from './CustomMap';
+
+export class User implements Mappable {
   name: string;
   location: {
     lat: number;
@@ -13,5 +15,9 @@ export class User {
       lat: parseFloat(faker.address.latitude()),
       lng: parseFloat(faker.address.longitude()),
     };
+  }
+
+  markerContent(): string {
+    return `User Name: ${this.name}`;
   }
 }
--- Modern-Angular-Bootcamp/15-typescript/03-map-app/src/index.ts
+++ Modern-Angular-Bootcamp/15-typescript/03-map-app/src/index.ts
@@ -1,5 +1,12 @@
+/// <reference types="@types/google.maps" />
+
 import { User } from './User';
 import { Company } from './Company';
+import { CustomMap } from './CustomMap';
 
-console.log(new User());
-console.log(new Company());
+const user = new User();
+const company = new Company();
+const map = new CustomMap('map');
+
+map.addMarker(user);
+map.addMarker(company);
