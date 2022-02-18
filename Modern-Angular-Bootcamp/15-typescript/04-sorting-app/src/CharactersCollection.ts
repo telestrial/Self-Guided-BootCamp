2b4cd88f8d2b88f08baf980a228174a76029c5b1
John Williams <telestrial@gmail.com>
2022-02-18 15:55:51 -0500

    Typescript V (417-44)

---
+++ Modern-Angular-Bootcamp/15-typescript/04-sorting-app/src/CharactersCollection.ts
@@ -0,0 +1,28 @@
+import { Sorter } from './Sorter';
+
+export class CharactersCollection extends Sorter {
+  constructor(public data: string) {
+    super();
+  }
+
+  get length(): number {
+    return this.data.length;
+  }
+
+  compare(leftIndex: number, rightIndex: number): boolean {
+    return (
+      this.data[leftIndex].toLowerCase() > this.data[rightIndex].toLowerCase()
+    );
+  }
+
+  swap(leftIndex: number, rightIndex: number): void {
+    const characters = this.data.split('');
+
+    [characters[leftIndex], characters[rightIndex]] = [
+      characters[rightIndex],
+      characters[leftIndex],
+    ];
+
+    this.data = characters.join('');
+  }
+}
