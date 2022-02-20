c3f0921f859553a31dafc5defc78733c75a80ff5
John Williams <telestrial@gmail.com>
2022-02-20 14:09:36 -0500

    Intro and Testing I (1-16)

---
+++ Advanced-React-Redux/01-testing/src/components/App.js
@@ -0,0 +1,13 @@
+import CommentList from './CommentList';
+import CommentBox from './CommentBox';
+
+const App = () => {
+  return (
+    <div>
+      <CommentBox />
+      <CommentList />
+    </div>
+  );
+};
+
+export default App;
---
+++ Advanced-React-Redux/01-testing/src/components/CommentList.js
@@ -0,0 +1,5 @@
+const CommentList = () => {
+  return <div>Comment List</div>;
+};
+
+export default CommentList;
