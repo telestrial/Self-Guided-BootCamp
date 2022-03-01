eb2bcd00a7c5d9de2c41f43f700406ac56bc7c4f
John Williams <telestrial@gmail.com>
2022-03-01 14:23:14 -0500

    Testing some concepts

---
+++ Sandbox/react-play/src/components/App.js
@@ -0,0 +1,15 @@
+import Container from './Container';
+import Card from './Card';
+
+import './App.css';
+
+const App = () => {
+  return (
+    <Container>
+      <h1>Random Card Thingy</h1>
+      <Card />
+    </Container>
+  );
+};
+
+export default App;
---
+++ Sandbox/react-play/src/components/Container.js
@@ -0,0 +1,7 @@
+import './Container.css';
+
+const Container = ({ children }) => {
+  return <div className="container">{children}</div>;
+};
+
+export default Container;
